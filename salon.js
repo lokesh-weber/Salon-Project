81a42289152ffe6d58b707cd58bf1408a724c906
lokesh-weber <sanodiyalokesh335@gmail.com>
2022-06-23 21:26:02 +0530

    final project

---
+++ salon.js
@@ -0,0 +1,21 @@
+var menu_btn = document.getElementById("menu_btn")
+var sidenav = document.getElementById("sidenav")
+var menu = document.getElementById("menu")
+
+sidenav.style.right = "-250px";
+
+menu_btn.onclick = function(){
+    if(sidenav.style.right == "-250px"){
+        sidenav.style.right = "0";
+        menu.src = "images/close.png";
+    }
+    else{
+        sidenav.style.right = "-250px";
+        menu.src = "images/menu.png";
+    }
+}
+
+var scroll = new SmoothScroll('a[href*="#"]',{
+    speed: 1000,
+    speedAsDuration:true
+});
